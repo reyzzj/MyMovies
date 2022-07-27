51ede80ce5e96443b4af318093a7cb95255dc1eb
reyzzj <21028514@myrp.edu.sg>
2022-07-27 11:57:03 +0800

    added new java classes and xml files

---
+++ app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/CustomAdapter.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.id21028514.mymovies;
+
+public class CustomAdapter {
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/ShowMovie.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21028514.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_movie);
+    }
+}
