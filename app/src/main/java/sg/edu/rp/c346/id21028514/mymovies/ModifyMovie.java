409b1774248d6fa220226748720a8303db5c5ca9
reyzzj <21028514@myrp.edu.sg>
2022-07-28 21:14:03 +0800

    clean code

--- app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/ModifyMovie.java
+++ app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/ModifyMovie.java
@@ -19,7 +19,6 @@ public class ModifyMovie extends AppCompatActivity {
     TextView tvID;
     EditText etEditContent, etEditContent2, etEditContent3;
     Button btnUpdate, btnDelete, btnCancel;
-    RadioButton rbEdit,starEdit1,starEdit2,starEdit3,starEdit4,starEdit5;
     RadioGroup rgEditContent;
     Movies data;
     Spinner spinGenreEdit;
@@ -36,17 +35,10 @@ public class ModifyMovie extends AppCompatActivity {
         etEditContent2 = findViewById(R.id.etEditContent2);
         etEditContent3 = findViewById(R.id.etEditContent3);
 
-        //starEdit1 = findViewById(R.id.radioButton1);
-        //starEdit2 = findViewById(R.id.radioButton2);
-        //starEdit3 = findViewById(R.id.radioButton3);
-        //starEdit4 = findViewById(R.id.radioButton4);
-        //starEdit5 = findViewById(R.id.radioButton5);
         btnUpdate = findViewById(R.id.btnUpdate);
         btnDelete = findViewById(R.id.btnDelete);
         btnCancel = findViewById(R.id.btnCancel);
         spinGenreEdit = findViewById(R.id.spinnerGenreEdit);
-
-
 
         Intent i = getIntent();
         data = (Movies) i.getSerializableExtra("data");
@@ -63,29 +55,7 @@ public class ModifyMovie extends AppCompatActivity {
                 data.setGenre(etEditContent2.getText().toString());
                 data.setYear(Integer.parseInt(etEditContent3.getText().toString()));
                 String movieRatingE = spinGenreEdit.getSelectedItem().toString();
-//                int data3 = rgEditContent.getCheckedRadioButtonId();
-//                rbEdit = findViewById(data3);
-//                String rating =(rbEdit.getText().toString());
-//                Log.d("result",rating+"");
-//
-//                Log.d("result",data3+"");
-//                int star = rgEditContent.getCheckedRadioButtonId();
-//                String radio = "";
-//
-//                if(star == R.id.radioEditButton1) {
-//                    radio = "*";
-//                } else if(star == R.id.radioEditButton2) {
-//                    radio = "**";
-//                } else if(star == R.id.radioEditButton3) {
-//                    radio = "***";
-//                } else if(star == R.id.radioEditButton4) {
-//                    radio = "****";
-//                } else if(star == R.id.radioEditButton5) {
-//                    radio = "*****";
-//                } else {
-//                    Toast.makeText(EditActivity.this, "Wrong star",
-//                            Toast.LENGTH_SHORT).show();
-//                }
+
 
                 data.setRating(movieRatingE);
 
