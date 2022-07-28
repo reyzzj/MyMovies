6ae4835331d93db262068c4bb7aa1f91299dc2d4
nrlandrini <21021749@myrp.edu.sg>
2022-07-28 22:18:47 +0800

    mostly changed in xml file, refactoring done

--- app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/ModifyMovie.java
+++ app/src/main/java/sg/edu/rp/c346/id21028514/mymovies/ModifyMovie.java
@@ -2,22 +2,21 @@ package sg.edu.rp.c346.id21028514.mymovies;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ModifyMovie extends AppCompatActivity {
 
     TextView tvID;
-    EditText etEditContent, etEditContent2, etEditContent3;
+    EditText etNewTitle, etNewGenre, etNewYear;
     Button btnUpdate, btnDelete, btnCancel;
     RadioButton rbEdit,starEdit1,starEdit2,starEdit3,starEdit4,starEdit5;
     RadioGroup rgEditContent;
@@ -32,36 +31,71 @@ public class ModifyMovie extends AppCompatActivity {
         //initialize the variables with UI here
 
         tvID = findViewById(R.id.tvID);
-        etEditContent = findViewById(R.id.etEditContent);
-        etEditContent2 = findViewById(R.id.etEditContent2);
-        etEditContent3 = findViewById(R.id.etEditContent3);
-
+        etNewTitle = findViewById(R.id.etMovieTitle);
+        etNewGenre = findViewById(R.id.etGenreName);
+        etNewYear = findViewById(R.id.etNewYear);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnCancel = findViewById(R.id.btnCancel);
+        spinGenreEdit = findViewById(R.id.spinnerGenreEdit);
         //starEdit1 = findViewById(R.id.radioButton1);
         //starEdit2 = findViewById(R.id.radioButton2);
         //starEdit3 = findViewById(R.id.radioButton3);
         //starEdit4 = findViewById(R.id.radioButton4);
         //starEdit5 = findViewById(R.id.radioButton5);
-        btnUpdate = findViewById(R.id.btnUpdate);
-        btnDelete = findViewById(R.id.btnDelete);
-        btnCancel = findViewById(R.id.btnCancel);
-        spinGenreEdit = findViewById(R.id.spinnerGenreEdit);
 
 
 
         Intent i = getIntent();
         data = (Movies) i.getSerializableExtra("data");
 
-        etEditContent.setText(data.getTitle());
-        etEditContent2.setText(data.getGenre());
-        etEditContent3.setText(data.getYear());
+        etNewTitle.setText(data.getTitle());
+        etNewGenre.setText(data.getGenre());
+        etNewYear.setText(data.getYear());
+
+        spinGenreEdit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+                if (data.getRating().equals("G")) {
+                    int position = 0;
+                    spinGenreEdit.setSelection(position);
+
+                } else if (data.getRating().equals("PG")) {
+                    int position = 1;
+                    spinGenreEdit.setSelection(position);
+
+                } else if (data.getRating().equals("PG13")) {
+                    int position = 2;
+                    spinGenreEdit.setSelection(position);
+
+                } else if (data.getRating().equals("NC16")) {
+                    int position = 3;
+                    spinGenreEdit.setSelection(position);
+
+                } else if (data.getRating().equals("M18")) {
+                    int position = 4;
+                    spinGenreEdit.setSelection(position);
+
+                } else if (data.getRating().equals("R21")) {
+                    int position = 5;
+                    spinGenreEdit.setSelection(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DBHelper dbh = new DBHelper(ModifyMovie.this);
-                data.setTitle(etEditContent.getText().toString());
-                data.setGenre(etEditContent2.getText().toString());
-                data.setYear(Integer.parseInt(etEditContent3.getText().toString()));
+                data.setTitle(etNewTitle.getText().toString());
+                data.setGenre(etNewGenre.getText().toString());
+                data.setYear(Integer.parseInt(etNewYear.getText().toString()));
                 String movieRatingE = spinGenreEdit.getSelectedItem().toString();
 //                int data3 = rgEditContent.getCheckedRadioButtonId();
 //                rbEdit = findViewById(data3);
